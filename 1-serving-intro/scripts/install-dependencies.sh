83e51fc3df40f67bc8e986de9454ad714be4cd70
Juan Sanin <jsanin@vmware.com>
2022-06-24 17:03:10 -0500

    Update of serving-intro to work on killercoda

--- 1-serving-intro/scripts/install-dependencies.sh
+++ 1-serving-intro/scripts/install-dependencies.sh
@@ -1,10 +1,10 @@
 #launch.sh
 
-kubectl apply -f https://github.com/knative/serving/releases/download/v0.22.3/serving-crds.yaml &> /dev/null
-kubectl apply -f https://github.com/knative/serving/releases/download/v0.22.3/serving-core.yaml &> /dev/null
+kubectl apply -f https://github.com/knative/serving/releases/download/knative-v1.4.0/serving-core.yaml &> /dev/null
+kubectl apply -f https://github.com/knative/serving/releases/download/knative-v1.4.0/serving-core.yaml &> /dev/null
 
-kubectl apply -f https://github.com/knative-sandbox/net-contour/releases/download/v0.22.1/contour.yaml &> /dev/null
-kubectl apply -f https://github.com/knative-sandbox/net-contour/releases/download/v0.22.1/net-contour.yaml &> /dev/null
+kubectl apply -f https://github.com/knative-sandbox/net-contour/releases/download/knative-v1.4.0/contour.yaml &> /dev/null
+kubectl apply -f https://github.com/knative-sandbox/net-contour/releases/download/knative-v1.4.0/net-contour.yaml &> /dev/null
 
 kubectl patch configmap/config-network \
   --namespace knative-serving \
@@ -17,7 +17,7 @@ externalIP=$(kubectl get service/envoy -n contour-external --output=jsonpath='{.
 echo "External IP assigned ${externalIP}"
 
 echo "Installing kn cli..."
-wget https://github.com/knative/client/releases/download/knative-v1.2.0/kn-linux-amd64 -O kn &> /dev/null
+wget https://github.com/knative/client/releases/download/knative-v1.4.1/kn-linux-amd64 -O kn &> /dev/null
 chmod +x kn &> /dev/null
 mv kn /usr/local/bin/ &> /dev/null
 echo "Done"
